f4a16909e17d876d2119ca22ae9c71e9d96bfda6
murali <muralibadiginti1999@gmail.com>
2025-05-16 23:50:47 +0530

    added data validation properties

---
+++ app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoalA : 'Finish the course and learn Vue!',
+            courseGoalB : 'Finish the course and Master Vue!',
+            vueLink : 'https://vuejs.org/'
+        }
+    },
+    methods : {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if(randomNumber<0.5) {
+                return this.courseGoalA
+            }else{
+                return this.courseGoalB
+            }
+        }
+    }
+})
+app.mount('#user-goal')
