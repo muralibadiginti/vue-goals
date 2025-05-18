c5a58a24f86d09055c227b011086264ee1a3691b
murali <muralibadiginti1999@gmail.com>
2025-05-18 21:03:56 +0530

    implemented computed properties

--- app.js
+++ app.js
@@ -2,27 +2,40 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name : '',
+      name: '',
     };
   },
-  methods : {
+  computed: {
+    fullname() {
+      if (this.name === '') {
+        this.name = 'max';
+      } else {
+        this.name = this.name + ' ' + 'Max'
+      }
+      return this.name
+    },
+  },
+  methods: {
+    outputFullName() {
+      return this.name + '' + 'Murali'
+    },
     setName(event) {
       this.name = event.target.value
     },
     add(num) {
-      this.counter+=num;
+      this.counter += num;
     },
-    reduce(num){
-      this.counter-=num;
+    reduce(num) {
+      this.counter -= num;
     }
-  ,
-  signUpForm(event) {
-    // event.preventDefault();
-    alert('Form submitted!!!')
-  },
-  resetInput() {
-    this.name = '';
-  }
+    ,
+    signUpForm(event) {
+      // event.preventDefault();
+      alert('Form submitted!!!')
+    },
+    resetInput() {
+      this.name = '';
+    }
   }
 });
 
